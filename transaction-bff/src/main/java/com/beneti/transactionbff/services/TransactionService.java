7f3095154fa19b5065328c9826c3842f66f04717
Guilherme S Beneti <guilhermesbeneti@Guilhermes-Air.local>
2023-02-14 09:10:23 -0300

    spring retry implemented

--- transaction-bff/src/main/java/com/beneti/transactionbff/services/TransactionService.java
+++ transaction-bff/src/main/java/com/beneti/transactionbff/services/TransactionService.java
@@ -3,6 +3,12 @@ package com.beneti.transactionbff.services;
 import com.beneti.transactionbff.dto.RequestTransactionDto;
 import com.beneti.transactionbff.dto.TransactionDto;
 import com.beneti.transactionbff.redis.TransactionRedisRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.QueryTimeoutException;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,9 +16,13 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class TransactionService {
 
     private TransactionRedisRepository transactionRedisRepository;
+
+    @Autowired
+    private RetryTemplate retryTemplate;
 
     public TransactionService(TransactionRedisRepository transactionRedisRepository) {
         this.transactionRedisRepository = transactionRedisRepository;
@@ -24,8 +34,17 @@ public class TransactionService {
         return Optional.of(transactionRedisRepository.save(requestTransactionDto));
     }
 
+//    @Retryable(value = QueryTimeoutException.class, maxAttempts = 5, backoff = @Backoff(delay = 100))
+//    public Optional<TransactionDto> findById(final String id) {
+//        log.info("Searching on Redis");
+//        return transactionRedisRepository.findById(id);
+//    }
+
     public Optional<TransactionDto> findById(final String id) {
-        return transactionRedisRepository.findById(id);
+        return retryTemplate.execute(ret -> {
+            log.info("Searching on Redis");
+            return transactionRedisRepository.findById(id);
+        });
     }
 
 }
